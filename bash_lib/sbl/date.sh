666a63af403fbffc6fa2210e03dffc551b8abe56
Shay Harding <3178267+kellewic@users.noreply.github.com>
2021-01-05 18:36:34 +0000

    Changed vim config for spaces/tabs since files had mixed and was causing alignment problems

--- bash_lib/sbl/date.sh
+++ bash_lib/sbl/date.sh
@@ -2,20 +2,20 @@ _sbl_date=1
 
 function date_to_stamp()
 {
-	## Assume the first arg is a date format string
-	local format="$1"
-	shift
-	local date_str="$*"
+    ## Assume the first arg is a date format string
+    local format="$1"
+    shift
+    local date_str="$*"
 
-	if [ -z "$date_str" ]; then
-		date_str="$format"
-	fi
+    if [ -z "$date_str" ]; then
+        date_str="$format"
+    fi
 
-	local date=$(date -u -d "$date_str" +%s 2>&-)
+    local date=$(date -u -d "$date_str" +%s 2>&-)
 
-	if [ -z "$date" ]; then
-		date=$(date -ju -f "$format" "$date_str" +%s 2>&-)
-	fi
+    if [ -z "$date" ]; then
+        date=$(date -ju -f "$format" "$date_str" +%s 2>&-)
+    fi
 
-	echo $date
+    echo $date
 }
--- bash_lib/sbl/lib.sh
+++ bash_lib/sbl/lib.sh
@@ -1,6 +1,6 @@
 ## Only run this if we haven't been sourced before
 if [ -z "$_sbl_lib" ]; then
-	declare -a _sbl_on_exit_commands
+    declare -a _sbl_on_exit_commands
 fi
 
 _sbl_lib=1
@@ -10,19 +10,19 @@ _sbl_lib=1
 function load_library()
 {
     local _my_path=$(dirname $0)/sbl
-	local lib
-	local lib_file
-	local loaded_lib
+    local lib
+    local lib_file
+    local loaded_lib
 
-	for file in "$@"; do
+    for file in "$@"; do
         lib=$(basename $file)
-		lib_file="$_my_path/$lib"
+        lib_file="$_my_path/$lib"
 
-		if [ -e "$lib_file.sh" ]; then
-			loaded_lib="_sbl_$lib"
-			[ -z "${!loaded_lib}" ] && . "$lib_file.sh"
-		fi
-	done
+        if [ -e "$lib_file.sh" ]; then
+            loaded_lib="_sbl_$lib"
+            [ -z "${!loaded_lib}" ] && . "$lib_file.sh"
+        fi
+    done
 }
 
 ## Exit functions taken from the following URL with only minor edits: 
@@ -31,19 +31,19 @@ function load_library()
 ## Run on-exit commands
 function on_exit()
 {
-	for i in "${_sbl_on_exit_commands[@]}"; do
-		eval $i
-	done
+    for i in "${_sbl_on_exit_commands[@]}"; do
+        eval $i
+    done
 }
 
 ## Add commands to execute on EXIT
 function on_exit_add()
 {
-	## Add command to exit array
-	local n=${#_sbl_on_exit_commands[*]}
-	_sbl_on_exit_commands[$n]="$*"
+    ## Add command to exit array
+    local n=${#_sbl_on_exit_commands[*]}
+    _sbl_on_exit_commands[$n]="$*"
 
-	## Set exit function on first call
-	[ $n -eq 0 ] && trap on_exit EXIT
+    ## Set exit function on first call
+    [ $n -eq 0 ] && trap on_exit EXIT
 }
 
